9b405050386733b188ad3cbc8977d65d86e44425
Arthur Stolyar <nekr.fabula@gmail.com>
2013-09-11 19:18:43 +0400

    separation compiler and runtime

--- sharp.js
+++ sharp.js
@@ -3,230 +3,230 @@
 
   var sharp = window.sharp || {};
 
-  sharp.runtime = {
-    render: function(tpl, data, partials) {
-      tpl = new Function('encodeHTML', sharp.varname, tpl);
-      return tpl(sharp.runtime.helpers.encodeHTML, data);
-    },
-    helpers: {
-      encodeHTML: function(str) {
-        return str.replace(R_MATCH_ENTITES, function(m) {
-          return entitesMap[m] || m;
-        });
-      }
-    }
-  };
+  sharp.VAR_NAME = '$';
+  sharp.HELPERS = 'h';
 
-  sharp.varname = '$';
-
-  sharp.compiler = {
-    settings: {
-      strip: true,
-      append: true
-    }
-  };
-
-  var OUTPUT_SIGN = 'out',
-    VAR_SIGN = 'v',
-    TMP_VAR = 'tmp',
-    STATEMENT = /(@BLOCK_CLOSE)?(@MAIN)(@IDENTIFIER)?/g,
-    consts = {
-      MAIN: /#/,
-      EXPR: /[\s\S]+/,
-      IDENTIFIER: /[A-Za-z_!:][A-Za-z_!:\d]*/,
-      BLOCK_OPEN: /\s*\{\s*/,
-      BLOCK_CLOSE: /\s*\}\s*/,
-      OUTPUT: new RegExp(OUTPUT_SIGN)
-      /*,
-      LOOK_BEHIND: /(?:(@CLOSE)[\s\S]*)?(@PATTERN)/g*/
-    },
-    tokens = {
-      '': {
-        pattern: /\{(@EXPR?)\}/,
-        open: function(compiler, code) {
-          return compiler.query(code);
-        },
-        interpolation: true,
-        hasUnsafe: true
-      },
-      'each': {
-        pattern: /\{\s*(@EXPR?)\s+->\s+([\w_]+?)\s*,\s*?([\w_]+?)\}?/,
-        open: function(compiler, iterate, key, value) {
-          var uVar = compiler.getVar(),
-            iterVar = compiler.getVar(),
-            valVar = compiler.getVar(),
-            keysVar = compiler.getVar('Object.keys');
-
-          iterate = compiler.query(iterate);
-
-          compiler.mapQuery(value, valVar)
-          compiler.mapQuery(key, uVar);
-
-          var str = 'var %iterVar% = %iterate%;' +
-                'if (%iterVar%) {' +
-                  '%keysVar%(%iterVar%).forEach(function(%uVar%) {' +
-                    '%valVar% = %iterVar%[%uVar%];';
-
-          return evalStr(str, {
-            uVar: uVar,
-            valVar: valVar,
-            iterVar: iterVar,
-            keysVar: keysVar,
-            iterate: iterate
-          });
-        },
-        close: function() {
-          return '});}';
-        }
-      },
-      'for': {
-        pattern: /\{\s*(@EXPR?)\s+->\s+([\w_]+?)\s*(?:,\s*?([\w_]+?))?\}/,
-        open: function(compiler, iterate, value, index) {
-          var uVar = compiler.getVar(),
-            iterVar = compiler.getVar(),
-            lenVar = compiler.getVar(),
-            valVar = compiler.getVar();
-
-          iterate = compiler.query(iterate);
-
-          compiler.mapQuery(value, valVar)
-          compiler.mapQuery(index, uVar);
-
-
-          var str = 'var %iterVar% = %iterate%;' +
-                'if (%iterVar%) {' +
-                  'for (var %uVar% = 0, %lenVar% = %iterVar%.length; %uVar% < %lenVar%; %uVar%++) {' +
-                    '%valVar% = %iterVar%[%uVar%];';
-
-          return evalStr(str, {
-            uVar: uVar,
-            iterVar: iterVar,
-            lenVar: lenVar,
-            valVar: valVar,
-            iterate: iterate
-          });
-        },
-        close: function() {
-          return '}}';
-        }
-      },
-      'if': {
-        pattern: /\{(@EXPR?)\}/,
-        open: function(compiler, expr) {
-          return 'if (' + compiler.query(expr) + ') {';
-        },
-        close: function() {
-          return '}';
-        }
-      },
-      'else': {
-        pattern: /(?:)/,
-        open: function() {
-          return 'else {'
-        },
-        close: function() {
-          return '}';
-        }
-      },
-      'elseif': {
-        pattern: /\{(@EXPR?)\}/,
-        open: function(compiler, expr) {
-          return "else if (" + compiler.query(expr) + ") {";
-        },
-        close: function() {
-          return '}';
-        }
-      },
-      json: {
-        pattern: /\{(@EXPR?)\}/,
-        open: function(compiler, expr) {
-          return 'JSON.stringify(' + compiler.query(expr) + ')';
-        },
-        interpolation: true,
-        hasUnsafe: true
-      }
+  (function sharpRuntime(){
+    var entitesMap = {
+      '&': '&#38;',
+      '<': '&#60;',
+      '>': '&#62;',
+      '"': '&#34;',
+      "'": '&#39;',
+      '/': '&#47;'
     };
 
-  var stream = {
-    open: function() {
-      return OUTPUT_SIGN + "+='";
+    var R_MATCH_ENTITES = /&(?!#?\w+;)|<|>|"|'|\//g;
+
+    sharp.runtime = {
+      render: function(tpl, data, partials) {
+        tpl = new Function(sharp.HELPERS, sharp.VAR_NAME, tpl);
+        return tpl(sharp.runtime.helpers, data);
+      },
+      helpers: {
+        encodeHTML: function(str) {
+          return str.replace(R_MATCH_ENTITES, function(m) {
+            return entitesMap[m] || m;
+          });
+        }
+      }
+    };
+  }());
+
+  (function sharpCompiler() {
+    var consts = {
+        MAIN: /#/,
+        EXPR: /[\s\S]+/,
+        IDENTIFIER: /[A-Za-z_!:][A-Za-z_!:\d]*/,
+        BLOCK_OPEN: /\s*\{\s*/,
+        BLOCK_CLOSE: /\s*\}\s*/,
+        EXPR_OPEN: /\{/,
+        EXPR_CLOSE: /\}(?![\s\S]*?\};);?/,
+        OUTPUT: new RegExp(OUTPUT_SIGN)
+        /*,
+        LOOK_BEHIND: /(?:(@CLOSE)[\s\S]*)?(@PATTERN)/g*/
+      },
+      tokens = {
+        '': {
+          pattern: /@EXPR_OPEN(@EXPR?)@EXPR_CLOSE/,
+          open: function(compiler, code) {
+            return compiler.query(code);
+          },
+          interpolation: true,
+          hasUnsafe: true
+        },
+        'each': {
+          pattern: /\{\s*(@EXPR?)\s+->\s+([\w_]+?)\s*,\s*?([\w_]+?)\}/,
+          open: function(compiler, iterate, key, value) {
+            var uVar = compiler.getVar(),
+              iterVar = compiler.getVar(),
+              valVar = compiler.getVar(),
+              keysVar = compiler.getVar('Object.keys');
+
+            iterate = compiler.query(iterate);
+
+            compiler.mapQuery(value, valVar)
+            compiler.mapQuery(key, uVar);
+
+            var str = 'var %iterVar% = %iterate%;' +
+                  'if (%iterVar%) {' +
+                    '%keysVar%(%iterVar%).forEach(function(%uVar%) {' +
+                      '%valVar% = %iterVar%[%uVar%];';
+
+            return evalStr(str, {
+              uVar: uVar,
+              valVar: valVar,
+              iterVar: iterVar,
+              keysVar: keysVar,
+              iterate: iterate
+            });
+          },
+          close: function() {
+            return '});}';
+          }
+        },
+        'for': {
+          pattern: /\{\s*(@EXPR?)\s+->\s+([\w_]+?)\s*(?:,\s*?([\w_]+?))?\}/,
+          open: function(compiler, iterate, value, index) {
+            var uVar = compiler.getVar(),
+              iterVar = compiler.getVar(),
+              lenVar = compiler.getVar(),
+              valVar = compiler.getVar();
+
+            iterate = compiler.query(iterate);
+
+            compiler.mapQuery(value, valVar)
+            compiler.mapQuery(index, uVar);
+
+
+            var str = 'var %iterVar% = %iterate%;' +
+                  'if (%iterVar%) {' +
+                    'for (var %uVar% = 0, %lenVar% = %iterVar%.length; %uVar% < %lenVar%; %uVar%++) {' +
+                      '%valVar% = %iterVar%[%uVar%];';
+
+            return evalStr(str, {
+              uVar: uVar,
+              iterVar: iterVar,
+              lenVar: lenVar,
+              valVar: valVar,
+              iterate: iterate
+            });
+          },
+          close: function() {
+            return '}}';
+          }
+        },
+        'if': {
+          pattern: /\{(@EXPR?)\}/,
+          open: function(compiler, expr) {
+            return 'if (' + compiler.query(expr) + ') {';
+          },
+          close: function() {
+            return '}';
+          }
+        },
+        'else': {
+          pattern: /(?:)/,
+          open: function() {
+            return 'else {'
+          },
+          close: function() {
+            return '}';
+          }
+        },
+        'elseif': {
+          pattern: /\{(@EXPR?)\}/,
+          open: function(compiler, expr) {
+            return "else if (" + compiler.query(expr) + ") {";
+          },
+          close: function() {
+            return '}';
+          }
+        },
+        'json': {
+          pattern: /@EXPR_OPEN(@EXPR?)@EXPR_CLOSE/,
+          open: function(compiler, expr) {
+            var stringifyVar = compiler.getVar('JSON.stringify');
+
+            return stringifyVar + '(' + compiler.query(expr) + ')';
+          },
+          interpolation: true,
+          hasUnsafe: true
+        }
+      };
+
+    var stream = {
+      open: function() {
+        return OUTPUT_SIGN + "+='";
+      },
+      close: function() {
+        return "';";
+      },
+      wrap: function(str) {
+        return this.close() + str + this.open();
+      },
+      interpolate: "function(data, safe) {" +
+        "var res = ((res = data) == null ? '' : res) + ''; return safe ? res : %encodeHTML%(res);}"
     },
-    close: function() {
-      return "';";
+    wrappers = {
+      append: "' + %interpolate%(%data%, %safe%) + '",
+      split: "'; out += %interpolate%(%data%, %safe%); out += '"
     },
-    wrap: function(wrapper, data, unsafe) {
-      return evalStr(unsafe ? wrapper.unsafe : wrapper.safe, {
-        data: data,
-        tmp: TMP_VAR
+    slice = Array.prototype.slice,
+    hasOwn = Object.prototype.hasOwnProperty;
+
+    var R_UNESCAPE_1 = /\\(['"\\])/g,
+      R_UNESCAPE_2 = /[\r\t\n]/g,
+      R_EVAL_STR = /%(\w+)%/g,
+      OUTPUT_SIGN = 'out',
+      VAR_SIGN = 'v',
+      TMP_VAR = 'tmp',
+      STATEMENT = /(@BLOCK_CLOSE)?(@MAIN)(@IDENTIFIER)?/g;
+
+    var unescape = function(code) {
+      return code.replace(R_UNESCAPE_1, '$1').replace(R_UNESCAPE_2, ' ');
+    },
+    getRegStr = function(reg) {
+      return reg.source;
+    },
+    evalReg = function(reg, flags) {
+      if (typeof reg !== 'string') {
+        reg = getRegStr(reg);
+      }
+
+      reg = reg.replace(/@(\w+)/g, function(input, _const) {
+        return getRegStr(consts[_const]);
       });
-    }
-  },
-  wrappers = {
-    append: {
-      safe: "' + encodeHTML(((%tmp% = %data%) == null ? '' : %tmp%) + '') + '",
-      unsafe: "' + (((%tmp% = %data%) == null ? '' : %tmp%) + '') + '"
+
+      reg = new RegExp(reg, typeof flags === 'string' ? flags : 'g');
+
+      return reg;
     },
-    split: {
-      safe: "'; out += encodeHTML(((%tmp% = %data%) == null ? '' : %tmp%) + ''); out += '",
-      unsafe: "'; out += (((%tmp% = %data%) == null ? '' : %tmp%) + ''); out += '"
-    }
-  },
-  entitesMap = {
-    '&': '&#38;',
-    '<': '&#60;',
-    '>': '&#62;',
-    '"': '&#34;',
-    "'": '&#39;',
-    '/': '&#47;'
-  },
-  slice = Array.prototype.slice,
-  hasOwn = Object.prototype.hasOwnProperty;
+    doReplace = function(string, from, to, replace) {
+      return string.slice(0, from) + replace + string.slice(to);
+    },
+    evalStr = function(str, data) {
+      return str.replace(R_EVAL_STR, function(input, key) {
+        return key.split('.').reduce(function(data, key) {
+          return data[key];
+        }, data);
+      });
+    };
 
-  var R_MATCH_ENTITES = /&(?!#?\w+;)|<|>|"|'|\//g,
-    R_UNESCAPE_1 = /\\(['"\\])/g,
-    R_UNESCAPE_2 = /[\r\t\n]/g,
-    R_EVAL_STR = /%(\w+)%/g;
+    sharp.compiler = {
+      settings: {
+        strip: true,
+        append: true
+      },
+      tokens: tokens
+    };
 
-  var unescape = function(code) {
-    return code.replace(R_UNESCAPE_1, '$1').replace(R_UNESCAPE_2, ' ');
-  },
-  getRegStr = function(reg) {
-    return reg.source;
-  },
-  evalReg = function(reg, flags) {
-    if (typeof reg !== 'string') {
-      reg = getRegStr(reg);
-    }
+    sharp.compiler.compile = function(str) {
+      var settings = sharp.compiler.settings,
+        wrapper = settings.append ? wrappers.append : wrappers.split,
+        uid = 0,
+        compiler;
 
-    reg = reg.replace(/@(\w+)/g, function(input, _const) {
-      return getRegStr(consts[_const]);
-    });
-
-    reg = new RegExp(reg, typeof flags === 'string' ? flags : 'g');
-
-    return reg;
-  },
-  doReplace = function(string, from, to, replace) {
-    return string.slice(0, from) + replace + string.slice(to);
-  },
-  evalStr = function(str, data) {
-    return str.replace(R_EVAL_STR, function(input, key) {
-      return key.split('.').reduce(function(data, key) {
-        return data[key];
-      }, data);
-    });
-  };
-
-  sharp.compiler.compile = function(str) {
-    var settings = sharp.compiler.settings,
-      wrapper = settings.append ? wrappers.append : wrappers.split,
-      uid = 0,
-      compiler;
-
-    str = (settings.strip ? str.replace(/(^|\r|\n)\t* +| +\t*(\r|\n|$)/g, ' ')
-          .replace(/\r|\n|\t|\/\*[\s\S]*?\*\//g, '') : str).replace(/'|"|\\/g, '\\$&');
-
-    (function() {
       var statementReg = evalReg(STATEMENT),
         match,
         matchStr,
@@ -241,7 +241,20 @@
         patternStart,
         patternMatchStr,
         main,
-        unsafe;
+        unsafe,
+        interpolateVar,
+        encodeHTMLVar;
+
+      var wrap = function(data, unsafe) {
+        return evalStr(wrapper, {
+          data: data,
+          safe: !unsafe,
+          interpolate: interpolateVar
+        });
+      };
+
+      str = (settings.strip ? str.replace(/(^|\r|\n)\t* +| +\t*(\r|\n|$)/g, ' ')
+            .replace(/\r|\n|\t|\/\*[\s\S]*?\*\//g, '') : str).replace(/'|"|\\/g, '\\$&');
 
       compiler = {
         getVar: function(toMap) {
@@ -255,7 +268,6 @@
 
           return defined;
         },
-        query: JSONQuery,
         mapQuery: function(who, by) {
           if (token.close) {
             //who = '$.' + who;
@@ -267,10 +279,16 @@
             this.queryMap[who] = by;
           }
         },
+        query: JSONQuery,
         queryMap: {},
         varMap: {},
         definedVars: []
       };
+
+      encodeHTMLVar = compiler.getVar(sharp.HELPERS + '.encodeHTML');
+      interpolateVar = compiler.getVar(evalStr(stream.interpolate, {
+        encodeHTML: encodeHTMLVar
+      }));
 
       statementReg.lastIndex = 0;
 
@@ -294,7 +312,7 @@
             tmp = token.close();
 
             if (!token.noWrap) {
-              tmp = stream.close() + tmp +  stream.open();
+              tmp = stream.wrap(tmp);
             }
 
             if (token.unmap && token.unmap.length) {
@@ -337,9 +355,9 @@
 
           if (!token.noWrap) {
             if (token.interpolation) {
-              tmp = stream.wrap(wrapper, tmp, token.hasUnsafe && unsafe);
+              tmp = wrap(tmp, token.hasUnsafe && unsafe);
             } else {
-              tmp = stream.close() + tmp + stream.open();
+              tmp = stream.wrap(tmp);
             }
           }
 
@@ -350,67 +368,71 @@
           );
         }
       }
-    }());
 
-    str = 'var ' + Object.keys(compiler.varMap).map(function(_var) {
-      return  this[_var] + ' = ' + _var;
-    }, compiler.varMap) + ';' +
-      "var " + TMP_VAR + ", " + OUTPUT_SIGN + "= '" + str + "'; return " + OUTPUT_SIGN + ";";
+      str = 'var ' + Object.keys(compiler.varMap).map(function(_var) {
+        return  this[_var] + ' = ' + _var;
+      }, compiler.varMap) + ';' + "var " + OUTPUT_SIGN + "= '" +
+        str + "'; return " + OUTPUT_SIGN + ";";
 
-    str = str.replace(/\n/g, '\\n').replace(/\t/g, '\\t').replace(/\r/g, '\\r')
-      .replace(evalReg(/(\s|;|\}|^|\{)@OUTPUT\+='';/), '$1').replace(/\+''/g, '')
-      .replace(evalReg(/(\s|;|\}|^|\{)@OUTPUT\+=''\+/),'$1' + OUTPUT_SIGN + '+=');
+      str = str.replace(/\n/g, '\\n').replace(/\t/g, '\\t').replace(/\r/g, '\\r')
+        .replace(evalReg(/(\s|;|\}|^|\{)@OUTPUT\+='';/), '$1').replace(/\+''/g, '')
+        .replace(evalReg(/(\s|;|\}|^|\{)@OUTPUT\+=''\+/),'$1' + OUTPUT_SIGN + '+=');
 
-    return str;
-  };
+      return str;
+    };
+
+    function JSONQuery(query, obj, args, context) {
+      var depth = 0,
+        strings = [],
+        executor,
+        compiler = this,
+        queryMap = this.queryMap;
+
+      query = unescape(query);
+
+      query = query.replace(/"(\\.|[^"\\])*"|'(\\.|[^'\\])*'|[\[\]]/g, function(t) {
+        depth += t === '[' ? 1 : (t === ']' ? -1 : 0); // keep track of bracket depth
+
+        return (t === ']' && depth > 0) ? '`]' : // we mark all the inner brackets as skippable
+            (t[0] === '"' || t[0] == "'") ? '`' + (strings.push(t) - 1) : t; // and replace all the strings
+      });
+
+      query = query.replace(/([^<>=]=)([^=])/g, '$1=$2'); // change the equals to comparisons except operators ==, <=, >=
+
+      query = query.replace(/(@|\.)?\s*([a-zA-Z_]+)(\s*:)?/g, function(str, sign, identifier, colon) {
+        if (colon) {
+          return (sign || '') + identifier + colon;
+        }
+
+        if (sign !== '.' && sign !== '@') {
+          if (!str.match(/^(\$([\da-zA-Z_]*)|Math|Date||parseInt|parseFloat|isNaN|isFinite|true|false|null)$/)) {
+            sign = '@';
+          }
+        }
+
+        if (sign === '@') {
+          if (hasOwn.call(queryMap, identifier)) {
+            return queryMap[identifier];
+          }
+
+          return sharp.VAR_NAME + '.' + identifier;
+        }
+
+        return (sign || '') + identifier;
+      });
+
+      /*query = query.replace(/\$(\d+|[a-z_][a-z0-9_]*)/gi, function(str, arg) {
+        return isFinite(arg) ? '_[' + arg + ']' : '_.' + arg;
+      });*/
+
+      query = query.replace(/`([0-9]+|\])/g, function(t, a) {
+        //restore the strings
+        return a === ']' ? ']' : strings[a];
+      });
+
+      return query;
+    };
+  }());
 
   window.sharp = sharp;
-
-  function JSONQuery(query, obj, args, context) {
-    var depth = 0,
-      strings = [],
-      executor,
-      compiler = this,
-      queryMap = this.queryMap;
-
-    query = unescape(query);
-
-    query = query.replace(/"(\\.|[^"\\])*"|'(\\.|[^'\\])*'|[\[\]]/g, function(t) {
-      depth += t === '[' ? 1 : (t === ']' ? -1 : 0); // keep track of bracket depth
-
-      return (t === ']' && depth > 0) ? '`]' : // we mark all the inner brackets as skippable
-          (t.charAt(0) === '"' || t.charAt(0) == "'") ? '`' + (strings.push(t) - 1) : t; // and replace all the strings
-    });
-
-    query = query.replace(/([^<>=]=)([^=])/g, '$1=$2') // change the equals to comparisons except operators ==, <=, >=
-
-    query = query.replace(/(@|\.)?\s*([a-zA-Z_]+)(\s*:)?/g, function(str, sign, identifier) {
-      if (sign !== '.' && sign !== '@') {
-        if (!str.match(/:|^(\$([\da-zA-Z_]*)|Math|Date|new Date|parseInt|parseFloat|isNaN|isFinite|true|false|null)$/)) {
-          sign = '@';
-        }
-      }
-
-      if (sign === '@') {
-        if (hasOwn.call(queryMap, identifier)) {
-          return queryMap[identifier];
-        }
-
-        return '$.' + identifier;
-      }
-
-      return sign + identifier;
-    });
-
-    query = query.replace(/\$(\d+|[a-z_][a-z0-9_]*)/gi, function(str, arg) {
-      return isFinite(arg) ? '_[' + arg + ']' : '_.' + arg;
-    });
-
-    query = query.replace(/`([0-9]+|\])/g, function(t, a) {
-      //restore the strings
-      return a === ']' ? ']' : strings[a];
-    });
-
-    return query;
-  };
 }(this));
